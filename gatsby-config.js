cbb89435360c6130941a4085b8b6c5320d8616b5
Preston So <pso@post.harvard.edu>
2021-10-04 10:54:46 -0300

    Add gatsby-plugin-algolia and dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
@@ -12,6 +14,14 @@ module.exports = {
     },
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        queries: require("./src/utils/algolia-queries")
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
